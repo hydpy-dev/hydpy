62622b837d470be5ffbc5fe5f494af6254ad37d6
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-03-09 13:55:04 +0100

    write html coverage report

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,3 +1,4 @@
 if [ $TRAVIS_PYTHON_VERSION = "2.7" ] && [ $TRAVIS_BRANCH = "master" ] && "$TRAVIS_PULL_REQUEST" == "false"]; then
   coverage run test_everything.py test_as_site-package
+  coverage html
 fi

8ad7d0fe667f3f36567b291173182cc5c4f0978d
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-03-14 08:33:52 +0100

    A small test for debugging on travis

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ if run_tests:
     oldpath = os.path.abspath('.')
     import hydpy.tests
     os.chdir(os.sep.join(hydpy.tests.__file__.split(os.sep)[:-1]))
+    os.system('ls')
     os.system('coverage run --branch --source hydpy test_everything.py')
     os.system('coverage report -m')
     os.system('coverage xml')

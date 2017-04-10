708efbcb6e74e66aa0e2e1f99dd39bf92cf8d023
HYDROLOGY\gordon <gordon.horn@rub.de>
2017-04-10 13:53:47 +0200

    ask for the operation system (Windows) first

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ from distutils.extension import Extension
 import Cython.Build
 import numpy
 
-from distutils import msvc9compiler
-if msvc9compiler.find_vcvarsall(9.0) is None:
-    raise NotImplementedError('ToDo')
+if sys.platform.startswith('win'):
+    from distutils import msvc9compiler
+    if msvc9compiler.find_vcvarsall(9.0) is None:
+        print("NotImplementedError('ToDo')")
 
 install = 'install' in sys.argv
 coverage_report = 'coverage_report' in sys.argv

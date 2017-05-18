78179be08d7507b735639f0de056bdc6b742b241
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-05-18 14:25:03 +0200

    Update globwat_links.py
    Each individual link sequence must inherit from hydpy.core.sequentools.LinkSequence.  Otherwise method as "setpointer" won't be available.

--- hydpy/models/globwat/globwat_links.py
+++ hydpy/models/globwat/globwat_links.py
@@ -9,7 +9,7 @@ from __future__ import division, print_function
 from hydpy.core import sequencetools
 
 
-class Q(sequencetools.Sequence):
+class Q(sequencetools.LinkSequence):
     """Runoff [m³/s]."""
     NDIM, NUMERIC = 0, False
 

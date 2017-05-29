ba5f310e38fec3e4626c28ff1f9b5fce5e5e57c8
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-05-29 12:48:07 +0200

    Add the update_inlets and update_outlets methods to HydPy-L-Lake.

--- hydpy/models/llake/llake_model.py
+++ hydpy/models/llake/llake_model.py
@@ -6,7 +6,23 @@ from __future__ import division, print_function
 # ...HydPy specific
 from hydpy.core import modeltools
 
+
+def update_inlets_v1(self):
+    """Update the inlet link sequence."""
+    flu = self.sequences.fluxes.fastaccess
+    inl = self.sequences.outlets.fastaccess
+    flu.qz = inl.q[0]
+
+
+def update_outlets_v1(self):
+    """Update the outlet link sequence."""
+    flu = self.sequences.fluxes.fastaccess
+    out = self.sequences.outlets.fastaccess
+    out.q[0] += flu.qa
+
+
 class Model(modeltools.Model):
     """Base model for HydPy-L-Lake."""
 
-    _RUNMETHODS = ()
+    _RUNMETHODS = (update_inlets_v1,
+                   update_outlets_v1)

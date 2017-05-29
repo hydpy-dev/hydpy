6196cafe0cb92d3a74f888b108edb0d6330d36c1
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-05-29 12:48:36 +0200

    Add llake_fluxes to HydPy-L-Lake.

---
+++ hydpy/models/llake/llake_fluxes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# import...
+# ...from standard library
+from __future__ import division, print_function
+# ...HydPy specific
+from hydpy.core import sequencetools
+
+
+class QZ(sequencetools.FluxSequence):
+    """Seezufluss (inflow into the lake) [m³/s]."""
+    NDIM, NUMERIC = 0, False
+
+
+class QA(sequencetools.FluxSequence):
+    """Seeausfluss (outflow from the lake) [m³/s]."""
+    NDIM, NUMERIC = 0, False
+
+
+class FluxSequences(sequencetools.FluxSequences):
+    """Flux sequences of HydPy-L-Lake."""
+    _SEQCLASSES = (QZ, QA)
+

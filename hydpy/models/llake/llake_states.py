8c052f4ac2af57bfaa84688e1c482e2fee340c9e
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-05-29 12:45:22 +0200

    add missing white lines to llake_states

--- hydpy/models/llake/llake_states.py
+++ hydpy/models/llake/llake_states.py
@@ -6,13 +6,16 @@ from __future__ import division, print_function
 # ...HydPy specific
 from hydpy.core import sequencetools
 
+
 class V(sequencetools.StateSequence):
     """Wasservolumen (water volume) [m³]."""
     NDIM, NUMERIC, SPAN = 0, False, (0., None)
 
+
 class W(sequencetools.StateSequence):
     """Wasserstand (water stage) [m]."""
     NDIM, NUMERIC, SPAN = 0, False, (None, None)
+
 
 class StateSequences(sequencetools.StateSequences):
     """State sequences of HydPy-L-Lake."""

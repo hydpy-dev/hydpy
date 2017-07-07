7d4154d95c3220efbd37d83cb161285b2e65dca7
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-07-07 13:55:13 +0200

    Some small improvements in module `apidoc` (local creation of the documentation).

--- hydpy/docs/apidoc.py
+++ hydpy/docs/apidoc.py
@@ -1,11 +1,12 @@
 
 import os
-import shutil
 import webbrowser
 
+os.system(r'make clean')
 os.system(r'make html')
 
-paths = [os.path.join('_build', 'html', fn) for fn in os.listdir(os.path.join('_build', 'html'))
+paths = [os.path.join('_build', 'html', fn)
+         for fn in os.listdir(os.path.join('_build', 'html'))
          if fn.endswith('.html')]
 for path in paths:
     lines = []
@@ -14,8 +15,9 @@ for path in paths:
             line = line.split('span')[1]
             line = line.split('>')[1]
             line = line.split('<')[0]
-            lines[-1] = lines[-1].replace('TYPE</code>',
-                                          'TYPE</code><em class="property"> = %s</em>' % line)
+            lines[-1] = lines[-1].replace(
+                    'TYPE</code>',
+                    'TYPE</code><em class="property"> = %s</em>' % line)
         else:
             lines.append(line)
     open(path, 'w').write(''.join(lines))
@@ -23,5 +25,5 @@ for path in paths:
 
 os.chdir(r'C:\Program Files (x86)\Mozilla Firefox')
 webbrowser.register('firefox', None, webbrowser.GenericBrowser('firefox'), 1)
-webbrowser.get('firefox').open_new_tab(r'C:\HydPy\hydpy\docs\_build\html\index.html')
-
+webbrowser.get('firefox').open_new_tab(
+                                r'C:\HydPy\hydpy\docs\_build\html\index.html')

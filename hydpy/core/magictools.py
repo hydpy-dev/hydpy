72f3ba2ab898211b04e7b6800d0fbf28219063df
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-08-17 15:47:36 +0200

    Add the iterator function `progressbar` to module `magictools`.
    This method can be used to print a simple progress while iterating arbitrary iterables.

--- hydpy/core/magictools.py
+++ hydpy/core/magictools.py
@@ -22,6 +22,7 @@ import warnings
 import importlib
 import doctest
 import functools
+import tempfile
 # ...from HydPy
 from hydpy import pub
 from hydpy.core import objecttools
@@ -58,6 +59,8 @@ class Tester(object):
         pub.options.usedefaultvalues = False
         printprogress = pub.options.printprogress
         pub.options.printprogress = False
+        printincolor = pub.options.printincolor
+        pub.options.printincolor = False
         warnsimulationstep = pub.options.warnsimulationstep
         pub.options.warnsimulationstep = False
         timegrids = pub.timegrids
@@ -98,6 +101,7 @@ class Tester(object):
         finally:
             pub.options.usedefaultvalues = usedefaultvalues
             pub.options.printprogress = printprogress
+            pub.options.printincolor = printincolor
             pub.options.warnsimulationstep = warnsimulationstep
             pub.timegrids = timegrids
             parametertools.Parameter._simulationstep = _simulationstep
@@ -113,15 +117,19 @@ class Tester(object):
 
 class PrintStyle(object):
 
-    def __init__(self, color, font):
+    def __init__(self, color, font, file=None):
         self.color = color
         self.font = font
+        self.file = sys.stdout if file is None else file
 
     def __enter__(self):
-        print('\x1B[%d;30;%dm' % (self.font, self.color))
+        if pub.options.printincolor:
+            print(end='\x1B[%d;30;%dm' % (self.font, self.color),
+                  file=self.file)
 
     def __exit__(self, exception, message, traceback_):
-        print('\x1B[0m')
+        if pub.options.printincolor:
+            print(end='\x1B[0m', file=self.file)
         if exception:
             objecttools.augmentexcmessage()
 
@@ -332,6 +340,117 @@ def printprogress(printprogress_wrapped):
     functools.update_wrapper(printprogress_wrapper_specialized,
                              printprogress_wrapped)
     return printprogress_wrapper_specialized
+
+
+def progressbar(iterable, length=23):
+    """Print a simple progress bar while processing the given iterable.
+
+    Function :func:`progressbar` does print the progress bar when option
+    `printprogress` is activted:
+
+    >>> from hydpy import pub
+    >>> pub.options.printprogress = True
+
+    You can pass an iterable object.  Say you want to calculate the the sum
+    of all integer values from 1 to 100 and print the progress of the
+    calculation.  Using function :func:`range` (which returns a list in
+    Python 2 and an iterator in Python3, but both are fine), one just has
+    to  interpose function :func:`progressbar`:
+
+    >>> from hydpy.core.magictools import progressbar
+    >>> x_sum = 0
+    >>> for x in progressbar(range(1, 101)):
+    ...     x_sum += x
+        |---------------------|
+        ***********************
+    >>> x_sum
+    5050
+
+    To prevent possible interim print commands from dismembering the status
+    bar, they are delayed until the status bar is complete.  For intermediate
+    print outs of each fiftieth calculation, the result looks as follows:
+
+    >>> x_sum = 0
+    >>> for x in progressbar(range(1, 101)):
+    ...     x_sum += x
+    ...     if not x % 50:
+    ...         print(x, x_sum)
+        |---------------------|
+        ***********************
+    50 1275
+    100 5050
+
+
+    The number of characters of the progress bar can be changed:
+
+    >>> for i in progressbar(range(100), length=50):
+    ...     continue
+        |------------------------------------------------|
+        **************************************************
+
+    But its maximum number of characters is restricted by the length of the
+    given iterable:
+
+    >>> for i in progressbar(range(10), length=50):
+    ...     continue
+        |--------|
+        **********
+
+    The smallest possible progress bar has two characters:
+
+    >>> for i in progressbar(range(2)):
+    ...     continue
+        ||
+        **
+
+    For iterables of length one or zero, no progress bar is plottet:
+
+
+    >>> for i in progressbar(range(1)):
+    ...     continue
+
+
+    The same is True when the `printprogress` option is inactivated:
+
+    >>> pub.options.printprogress = False
+    >>> for i in progressbar(range(100)):
+    ...     continue
+    """
+    if pub.options.printprogress and (len(iterable) > 1):
+        temp_name = os.path.join(tempfile.gettempdir(),
+                                 'HydPy_progressbar_stdout')
+        temp_stdout = open(temp_name, 'w')
+        real_stdout = sys.stdout
+        try:
+            sys.stdout = temp_stdout
+            nmbstars = min(len(iterable), length)
+            nmbcounts = len(iterable)/nmbstars
+            indentation = ' '*max(pub._printprogress_indentation, 0)
+            with PrintStyle(color=36, font=1, file=real_stdout):
+                print('    %s|%s|\n%s    ' % (indentation,
+                                              '-'*(nmbstars-2),
+                                              indentation),
+                      end='',
+                      file=real_stdout)
+                counts = 1
+                for next_ in iterable:
+                    counts += 1
+                    if counts >= nmbcounts:
+                        print(end='*', file=real_stdout)
+                        counts = 1
+                    yield next_
+        finally:
+            try:
+                temp_stdout.close()
+            except BaseException:
+                pass
+            sys.stdout = real_stdout
+            print()
+            with open(temp_name, 'r') as temp_stdout:
+                sys.stdout.write(temp_stdout.read())
+    else:
+        for next_ in iterable:
+            yield next_
 
 
 autodoctools.autodoc_module()

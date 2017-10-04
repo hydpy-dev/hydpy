05a0dd6babdf1b55eda320212148c8557cf6e831
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-10-04 10:18:00 +0200

    Add a `Config` instance of module `configtools` to the global data module `pub`.

--- hydpy/pub.py
+++ hydpy/pub.py
@@ -2,14 +2,15 @@
 # import from...
 # ...the standard library
 import sys as __sys
-# from HydPy
-from hydpy.core import objecttools
-from hydpy.core import indextools
+# ...from HydPy
+from hydpy.core import objecttools as __objecttools
+from hydpy.core import indextools as __indextools
+from hydpy.cythons import configtools as __configtools
 
 projectname = None
 
-options = objecttools.Options()
-indexer = indextools.Indexer()
+options = __objecttools.Options()
+indexer = __indextools.Indexer()
 networkmanager = None
 controlmanager = None
 conditionmanager = None
@@ -18,3 +19,5 @@ timegrids = None
 
 pyversion = int(__sys.version[0])
 _printprogress_indentation = -4
+
+config = __configtools.Config()

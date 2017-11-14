3cce87b1a730bb041de227078e5dc7d80a996b80
Christoph Tyralla <Christoph.Tyralla@rub.de>
2017-11-14 16:36:35 +0100

    Make method `autodoc_module` of module `autodoctools` compatible with PyInstaller.
    Function `getmodule` of module `inspect` returned `None` in line `module = inspect.getmodule(inspect.currentframe().f_back)` when PyInstaller has been used to freeze HydPy.
    
    It should be possible to solve this problem with not too much effort.  But for now, it should be ok to just check for `None` and eventually abort the function `autodoc_module`.  I do net see a reason at the moment, why the final executable would need extended module docstrings.

--- hydpy/core/autodoctools.py
+++ hydpy/core/autodoctools.py
@@ -180,27 +180,32 @@ def autodoc_module():
     :func:`autodoc_basemodel` instead.
     """
     module = inspect.getmodule(inspect.currentframe().f_back)
-    doc = module.__doc__
-    if doc is None:
-        doc = ''
-    lines = ['\n\nModule :mod:`~%s` implements the following members:\n'
-             % module.__name__]
-    members = []
-    for (name, member) in inspect.getmembers(module):
-        if ((not name.startswith('_')) and
-                (inspect.getmodule(member) is module)):
-            members.append((name, member))
-    members = sorted(members, key=_number_of_line)
-    for (name, member) in members:
-        if inspect.isfunction(member):
-            type_ = 'func'
-        elif inspect.isclass(member):
-            type_ = 'class'
-        else:
-            type_ = 'object'
-        lines.append('      * :%s:`~%s` %s'
-                     % (type_, name, description(member)))
-    module.__doc__ = doc + '\n\n' + '\n'.join(lines) + '\n\n' + 80*'_'
+    if module is None:
+        # Happens when HydPy has been freezed with PyInstaller.
+        # But then it is not necessary to extend the docstring anyway.
+        return
+    else:
+        doc = module.__doc__
+        if doc is None:
+            doc = ''
+        lines = ['\n\nModule :mod:`~%s` implements the following members:\n'
+                 % module.__name__]
+        members = []
+        for (name, member) in inspect.getmembers(module):
+            if ((not name.startswith('_')) and
+                    (inspect.getmodule(member) is module)):
+                members.append((name, member))
+        members = sorted(members, key=_number_of_line)
+        for (name, member) in members:
+            if inspect.isfunction(member):
+                type_ = 'func'
+            elif inspect.isclass(member):
+                type_ = 'class'
+            else:
+                type_ = 'object'
+            lines.append('      * :%s:`~%s` %s'
+                         % (type_, name, description(member)))
+        module.__doc__ = doc + '\n\n' + '\n'.join(lines) + '\n\n' + 80*'_'
 
 
 autodoc_module()

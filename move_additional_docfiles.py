f89aadd833d843631ff182897c18bf2850423705
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-02-18 14:57:23 +0100

    Try to fix commit #db1cfe0.
    Now, html file moving should be performed on the hydpy site-package.

--- move_additional_docfiles.py
+++ move_additional_docfiles.py
@@ -2,6 +2,15 @@
 
 import os
 import shutil
+import sys
+
+# Priorise site-packages (on Debian-based Linux distributions as Ubuntu
+# also dist-packages) in the import order to make sure, the following
+# imports refer to the newly build hydpy package on the respective computer.
+paths = [path for path in sys.path if path.endswith('-packages')]
+for path in paths:
+    sys.path.insert(0, path)
+
 from hydpy import docs
 folder_out = docs.__path__[0]
 folder_in = os.path.join(folder_out, 'html')

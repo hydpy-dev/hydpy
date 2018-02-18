db1cfe0aca2654858ebc982b2ad7ee4978d3425f
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-02-18 14:03:38 +0100

    Show bokeh files on Github-pages (second try).
    I think, `travis-sphinx` copies the files contained in the `sources` directory only, not those contained in subdirectories of `sources`.  Hence, this commit adds the script `move_additional_docfiles.py`, which moves all files from directory `doc/html` to `doc`.

---
+++ move_additional_docfiles.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+from hydpy import docs
+folder_out = docs.__path__[0]
+folder_in = os.path.join(folder_out, 'html')
+for filename in os.listdir(folder_in):
+    if filename.endswith('.html'):
+        path_in = os.path.join(folder_in, filename)
+        path_out = os.path.join(folder_out, filename)
+        shutil.move(path_in, path_out)

68c4d4d5f6bccfe69f6fe2d560859778538f2123
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-04-19 22:23:25 +0200

    Add module `lland_logs` (missing in commit #af4a91c).

---
+++ hydpy/models/lland/lland_logs.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# import...
+# ...from standard library
+from __future__ import division, print_function
+# ...HydPy specific
+from hydpy.core import sequencetools
+
+
+class WET0(sequencetools.LogSequence):
+    """Zeitlich gewichtete Grasreferenzverdunstung (temporally weighted
+    reference evapotranspiration) [mm].
+
+    Log sequence |WET0| is generally initialized with a length of one
+    on the first axis:
+
+    >>> from hydpy.models.lland import *
+    >>> parameterstep()
+    >>> logs.wet0.shape = 3
+    >>> logs.wet0.shape
+    (1, 3)
+    """
+    NDIM, NUMERIC = 2, False
+
+    def _setshape(self, shape):
+        super(WET0, self)._setshape((1, shape))
+
+    shape = property(sequencetools.LogSequence._getshape, _setshape)
+
+
+class LogSequences(sequencetools.LogSequences):
+    """Log sequences of the HydPy-L-Land model."""
+    _SEQCLASSES = (WET0,)

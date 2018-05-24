0ce73ea1285c06abe11c99352eae4e018b0fa170
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-05-24 10:56:21 +0200

    Move the files controlling `sphinx` to the new folder `sphinx`.

--- hydpy/docs/sphinx/conf.py
+++ hydpy/docs/sphinx/conf.py
@@ -22,9 +22,6 @@ sys.path.insert(0, os.path.abspath('..\\..\\'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
-from hydpy import docs
-filename = os.path.join('C:\\temp', 'rst_prolog.temp')
-rst_prolog = open(filename).read()
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.

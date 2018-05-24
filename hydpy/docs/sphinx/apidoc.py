77c9c7b29318499cc49cfcf9258bd465e9f5ea6f
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-05-24 11:41:12 +0200

    Updatede the `apidoc` script.

--- hydpy/docs/sphinx/apidoc.py
+++ hydpy/docs/sphinx/apidoc.py
@@ -5,25 +5,27 @@ import webbrowser
 os.system(r'make clean')
 os.system(r'make html')
 
-paths = [os.path.join('_build', 'html', fn)
-         for fn in os.listdir(os.path.join('_build', 'html'))
+folder = os.path.join('auto', '_build', 'html')
+paths = [os.path.join(folder, fn) for fn in os.listdir(folder)
          if fn.endswith('.html')]
 for path in paths:
-    lines = []
-    for line in open(path):
-        if line.startswith('<dd><p>alias of <a class="reference external"'):
-            line = line.split('span')[1]
-            line = line.split('>')[1]
-            line = line.split('<')[0]
-            lines[-1] = lines[-1].replace(
-                    'TYPE</code>',
-                    'TYPE</code><em class="property"> = %s</em>' % line)
-        else:
-            lines.append(line)
-    open(path, 'w').write(''.join(lines))
-
+        lines = []
+        with open(path) as file_:
+            for line in file_:
+                if line.startswith('<dd><p>alias of <a '
+                                   'class="reference external"'):
+                    line = line.split('span')[1]
+                    line = line.split('>')[1]
+                    line = line.split('<')[0]
+                    lines[-1] = lines[-1].replace(
+                           'TYPE</code>',
+                           'TYPE</code><em class="property"> = %s</em>' % line)
+                else:
+                    lines.append(line)
+        with open(path, 'w') as file_:
+            file_.write(''.join(lines))
 
 os.chdir(r'C:\Program Files (x86)\Mozilla Firefox')
 webbrowser.register('firefox', None, webbrowser.GenericBrowser('firefox'), 1)
 webbrowser.get('firefox').open_new_tab(
-                                r'C:\HydPy\hydpy\docs\_build\html\index.html')
+    r'C:\HydPy\hydpy\docs\auto\_build\html\index.html')

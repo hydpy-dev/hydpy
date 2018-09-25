acdbd71a6f57ab9f190367aba2eb4530393c87be
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-09-25 14:27:19 +0200

    Let `HydPy2FEWS.py` run when imported by `runpy`.

--- hydpy/scripts/HydPy2FEWS.py
+++ hydpy/scripts/HydPy2FEWS.py
@@ -3,10 +3,9 @@
 import logging
 import sys
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='HydPy2FEWS.log', level=logging.INFO)
-    try:
-        from hydpy.auxs.xmltools import execute_workflow
-        execute_workflow(argv=sys.argv)
-    except BaseException as exc:
-        logging.critical('Failed')
+logging.basicConfig(filename='HydPy2FEWS.log', level=logging.INFO)
+try:
+    from hydpy.auxs.xmltools import execute_workflow
+    execute_workflow(argv=sys.argv)
+except BaseException as exc:
+    logging.critical('Failed')

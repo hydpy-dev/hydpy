c4b8ba8943542aaedb5df04a809dca4aafdd11c6
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-09-27 19:24:33 +0200

    Add function `exec_commands` and use it to improve the error/warning message services of `hyd.py` and test it more rigorously.

--- hydpy/exe/hyd.py
+++ hydpy/exe/hyd.py
@@ -6,26 +6,26 @@ https://docs.python.org/3/using/windows.html#launcher
 
 .. _`here`: https://bitbucket.org/vinay.sajip/pylauncher/downloads//
 
-This script is thought to be called from a command line.  After successful
-installation of HydPy, you should be able to invoke it from anywhere on
-your computer.  You can test this by just typing `hyd.py` into your command
-line prompt.  Then you should find a HydPy log file within your current
-working directory, containing the current date in its name.  The following
-example shows this by using class |TestIO|, to write the log file into
-the `iotesting` folder:
+This script is thought to be called from a command line prompt.  After
+successful installation of HydPy, you should be able to invoke it from a
+nywhere on your computer.  You can test this by just typing `hyd.py` into
+your command line prompt.  Then you should find a HydPy log file within
+your current working directory, containing the current date in its name.
+The following example shows this by using class |TestIO|, to write the
+log file into the `iotesting` folder:
 
->>> import subprocess, sys
->>> from hydpy import TestIO, execute_scriptfunction, print_latest_logfile
+>>> import subprocess
+>>> from hydpy import TestIO, print_latest_logfile
 >>> TestIO.clear()
->>> with TestIO(clear_own=True):
+>>> with TestIO():
 ...     print_latest_logfile()
 Traceback (most recent call last):
 ...
 FileNotFoundError: Cannot find a HydPy log file in directory ...iotesting.
->>> with TestIO(clear_own=True):
+>>> with TestIO():
 ...     _ = subprocess.call('hyd.py', shell=True)
 ...     print_latest_logfile()
-Invoking hyd.py with arguments ...hyd.py` resulted in the following error:
+Invoking hyd.py with arguments `...hyd.py` resulted in the following error:
 The first argument defining the function to be called is missing.
 <BLANKLINE>
 
@@ -37,67 +37,89 @@ installed.  The Python standard distribution contains this launcher, but
 other distributions like Anaconda do not.  You can find the suitable
 installer `here`_.
 
-Upon invoking `hyd.py`, it calls |execute_scriptfunction|.  We will use
-this function directly in the following examples.  For convenience, we
-wrap |execute_scriptfunction| and also |print_latest_logfile| within
-test function `execute`:
+For convenience, we wrap the three required code lines test function `execute`:
 
 >>> def execute(command):
-...     sys.argv = command.split()
-...     with TestIO(clear_own=True):
-...         execute_scriptfunction()
+...     with TestIO():
+...         _ = subprocess.call(command, shell=True)
 ...         print_latest_logfile()
 
-Without any further arguments, `hyd.py` does not know which function to call
-(note that the content of the logfile exactly the same as in the example
-above making a system call):
+Without any further arguments, `hyd.py` does not know which function to call:
 
->>> execute('hyd.py')
-Invoking hyd.py with arguments `hyd.py` resulted in the following error:
+>>> execute("hyd.py")
+Invoking hyd.py with arguments `...hyd.py` resulted in the following error:
 The first argument defining the function to be called is missing.
 <BLANKLINE>
 
 The first additional argument must be an available "script function":
 
->>> execute('hyd.py wrong_argument')
-Invoking hyd.py with arguments `hyd.py, wrong_argument` resulted in the \
+>>> execute("hyd.py "
+...         "wrong_argument")
+Invoking hyd.py with arguments `...hyd.py, wrong_argument` resulted in the \
 following error:
 There is no `wrong_argument` function callable by `hyd.py`.  \
-Choose one of the following instead: exec_xml
+Choose one of the following instead: exec_xml and exec_commands
 <BLANKLINE>
 
 Further argument requirements depend on the selected "script function":
 
->>> execute('hyd.py exec_xml')
-Invoking hyd.py with arguments `hyd.py, exec_xml` resulted in the \
+>>> execute("hyd.py "
+...         "exec_commands")
+Invoking hyd.py with arguments `...hyd.py, exec_commands` resulted in the \
 following error:
-Function `exec_xml` requires `1` arguments (projectname), but `0` are given.
+Function `exec_commands` requires `1` arguments (commands), but `0` are given.
 <BLANKLINE>
->>> execute('hyd.py exec_xml first_name second_name')
-Invoking hyd.py with arguments `hyd.py, exec_xml, first_name, \
+>>> execute("hyd.py "
+...         "exec_commands "
+...         "first_name "
+...         "second_name")
+Invoking hyd.py with arguments ...hyd.py, exec_commands, first_name, \
 second_name` resulted in the following error:
-Function `exec_xml` requires `1` arguments (projectname), but `2` are given \
+Function `exec_commands` requires `1` arguments (commands), but `2` are given \
 (first_name and second_name).
 <BLANKLINE>
 
 Error messages raised by the "script function" itself also find their
 way into the log file:
 
->>> execute('hyd.py exec_xml LahnHBV')
-Start HydPy project `LahnHBV` (...).
-Read configuration file `conf.xml` (...).
-Invoking hyd.py with arguments `hyd.py, exec_xml, LahnHBV` resulted \
-in the following error:
-While trying to read parse the xml configuration file ...config.xml, \
-the following error occurred: [Errno 2] No such file or directory: \
-'LahnHBV...config.xml'
+>>> execute("hyd.py "
+...         "exec_commands "
+...         "raise_RuntimeError('it_fails')")
+Start to execute the commands ["raise_RuntimeError('it_fails')"] for \
+testing purposes.
+Invoking hyd.py with arguments `...hyd.py, exec_commands, \
+raise_RuntimeError('it_fails')` resulted in the following error:
+it fails
 <BLANKLINE>
 
+The same is true for warning messages:
+
+>>> execute("hyd.py "
+...         "exec_commands "
+...         "import_warnings;"
+...         "warnings.warn('it_stumbles')")
+Start to execute the commands ['import_warnings', \
+"warnings.warn('it_stumbles')"] for testing purposes...
+...UserWarning: it stumbles
+  #!python3
+<BLANKLINE>
+
+Each "script function" is allowed to write additional information
+into the logging file:
+
+>>> execute("hyd.py "
+...         "exec_commands "
+...         "logfile.write('it_works')")
+Start to execute the commands ["logfile.write('it_works')"] \
+for testing purposes.
+it works
+
 See the documentation on module |xmltools| for an actually successful
-invokation of `exec_xml`.
+example using the "script function" |exec_xml|.
 """
 # import...
 # ...from standard library
+from typing import IO
 import inspect
 import os
 import sys
@@ -106,6 +128,29 @@ import time
 from hydpy import pub
 from hydpy.core import autodoctools
 from hydpy.core import objecttools
+
+
+def exec_commands(commands, *, logfile: IO) -> None:
+    """Execute the given Python.
+
+    Function |exec_commands| is thought for testing purposes only (see
+    the main documentation on module |hyd|).  The given commands must
+    be seperated by semicolons, underscores are replaced by whitespaces:
+
+    >>> from hydpy.exe.hyd import exec_commands
+    >>> import sys
+    >>> exec_commands("x_=_1+1;print(x)", logfile=sys.stdout)
+    Start to execute the commands ['x_=_1+1', 'print(x)'] for testing purposes.
+    2
+    """
+    cmdlist = commands.split(';')
+    logfile.write(
+        f'Start to execute the commands {cmdlist} for testing purposes.\n')
+    for command in cmdlist:
+        exec(command.replace('_', ' '))
+
+
+pub.scriptfunctions['exec_commands'] = exec_commands
 
 
 def print_latest_logfile(dirpath='.'):
@@ -128,7 +173,8 @@ def print_latest_logfile(dirpath='.'):
 def execute_scriptfunction():
     """Execute a HydPy script function.
 
-    See the main documentation on module |hyd| for more information.
+    Function |exectue_scriptfunction| is indirectly applied in the
+    examples of the main documentation on module |hyd|.
     """
     logfilename = time.strftime('hydpy_%Y-%m-%d_%H-%M-%S.log')
     with open(logfilename, 'w'):
@@ -166,8 +212,13 @@ def execute_scriptfunction():
                 f'Function `{funcname}` requires `{nmb_args_required:d}` '
                 f'arguments{enum_args_required}, but `{nmb_args_given:d}` '
                 f'are given{enum_args_given}.')
-        with open(logfilename, 'a') as logfile:
-            func(*sys.argv[2:], logfile=logfile)
+        stdout = sys.stdout
+        try:
+            with open(logfilename, 'a') as logfile:
+                sys.stdout = logfile
+                func(*sys.argv[2:], logfile=logfile)
+        finally:
+            sys.stdout = stdout
     except BaseException as exc:
         with open(logfilename, 'a') as logfile:
             arguments = ', '.join(sys.argv)

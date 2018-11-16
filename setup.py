cff5f40435cb092474d2136571cd8984d51d5456
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-11-16 11:08:02 +0100

    Go to version 3.0.1 for fixing application model `hstream_v1`.

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ with open("README.rst", "r") as readmefile:
 
 # The usual setup definitions.
 setup(name='HydPy',
-      version='3.0.0',
+      version='3.0.1',
       description='A framework for the development and application of '
                   'hydrological models.',
       long_description=long_description,

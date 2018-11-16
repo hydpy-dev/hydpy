ea76eb7e991aed6ef6a60e1531a80a5da97f6d91
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-11-16 16:37:45 +0100

    Make `sitecustomize.py` more verbose.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,2 +1,10 @@
 import coverage
+import os
+cps = os.environ.get('COVERAGE_PROCESS_START')
+if cps is None:
+    print('\nStarting Python process without coverage measurement.\n')
+else:
+    print(f'\nStarting Python process with coverage measurement based '
+          f'on configuration file `{cps}`.  Look for the result file '
+          f'in directory `{os.path.abspath(".")}`.\n')
 coverage.process_startup()

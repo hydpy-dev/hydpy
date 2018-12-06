5c4414d77ffc42302478b3aecd656981b9c0d9b6
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-12-06 10:23:54 +0100

    Silence `sitecustomize.py`.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,10 +1,4 @@
-import coverage
 import os
-cps = os.environ.get('COVERAGE_PROCESS_START')
-if cps is None:
-    print('\nStarting Python process without coverage measurement.\n')
-else:
-    print(f'\nStarting Python process with coverage measurement based '
-          f'on configuration file `{cps}`.  Look for the result file '
-          f'in directory `{os.path.abspath(".")}`.\n')
-coverage.process_startup()
+if os.environ.get('COVERAGE_PROCESS_START') is not None:
+    import coverage
+    coverage.process_startup()

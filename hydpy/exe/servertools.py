3fd0b7f4b5c3c4aca8d49d5e09d248a2a9fb5032
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-12-07 13:55:29 +0100

    Let the temporary method `post_process_input` send and receive data file contents via HTTP requests instead of file paths.

--- hydpy/exe/servertools.py
+++ hydpy/exe/servertools.py
@@ -15,24 +15,19 @@
 >>> from urllib import request
 >>> from hydpy import print_values
 >>> def test(dirname, time_, alpha):
-...     with TestIO():
-...         if not os.path.exists(dirname):
-...             os.mkdir(dirname)
-...         dirpath = os.path.abspath(dirname).encode('utf-8')
-...         with open(f'{dirname}/hydpy.exchange', 'w') as infile:
-...             _ = infile.write(f'alpha= [{alpha}] \\n')
-...             _ = infile.write("refdate= '01 jan 1996' \\n")
-...             _ = infile.write("unit= '1d' \\n")
-...             _ = infile.write(f'time={time_} \\n')
-...             _ = infile.write('dill.discharge=[0.0] \\n')
-...             _ = infile.write('lahn_1.discharge=[0.0] \\n')
-...         _ = request.urlopen(
-...             'http://localhost:8080/process_input', data=dirpath)
-...         with open(f'{dirname}/hydpy.exchange') as outfile:
-...             for line in outfile.readlines():
-...                 if line.startswith('dill.discharge'):
-...                     values = eval(line.split('=')[1])
-...                     print_values(values)
+...     content = (f"alpha= [{alpha}] \\n"
+...                f"refdate= '01 jan 1996' \\n"
+...                f"unit= '1d' \\n"
+...                f"time={time_} \\n"
+...                f"dill.discharge=[0.0] \\n"
+...                f"lahn_1.discharge=[0.0]").encode('utf-8')
+...     response = request.urlopen(
+...             f'http://localhost:8080/process_input?id={dirname}', data=content)
+...     lines = str(response.read(), encoding='utf-8').split('\\n')
+...     for line in lines:
+...         if line.startswith('dill.discharge'):
+...             values = eval(line.split('=')[1])
+...             print_values(values)
 
 Single simulation run:
 
@@ -76,10 +71,10 @@ Parallel runs with different parameterisations:
 # ...from standard library
 import collections
 import http.server
-import os
 import threading
 import time
 import urllib.error
+import urllib.parse
 import urllib.request
 # ...from HydPy
 from hydpy import pub
@@ -97,8 +92,12 @@ class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            externalname = self.path[1:]
+            externalname = urllib.parse.urlparse(self.path).path[1:]
             internalname = f'get_{externalname}'
+            try:
+                self.server.id_ = urllib.parse.parse_qsl( self.path)[0][1]
+            except IndexError:
+                self.server.id_ = None
             try:
                 method = getattr(self, internalname)
             except AttributeError:
@@ -116,24 +115,27 @@ class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            externalname = self.path[1:]
+            externalname = urllib.parse.urlparse(self.path).path[1:]
             internalname = f'post_{externalname}'
+            content_length = int(self.headers['Content-Length'])
+            self.server.id_ = urllib.parse.parse_qsl(self.path)[0][1]
+            self.server.inlines = str(
+                self.rfile.read(content_length), encoding='utf-8').split('\n')
+            self.server.outlines = []
             try:
                 method = getattr(self, internalname)
             except AttributeError:
                 raise AttributeError(
                     f'No POST method for property `{externalname}` available.')
             try:
-                method(post_data)
+                method()
             except BaseException:
                 objecttools.augment_excmessage(
                     f'While trying execute the POST method '
                     f'of property {externalname}')
             self._set_headers()
-            self.wfile.write(
-                bytes(f'POST request for {self.path}', encoding='utf-8'))
+            output = '\n'.join(self.server.outlines)
+            self.wfile.write(bytes(output, encoding='utf-8'))
         except BaseException as exc:
             self._set_headers()
             self.wfile.write(bytes(f'{type(exc)}: {exc}', encoding='utf-8'))
@@ -149,20 +151,16 @@ class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         shutter.deamon = True
         shutter.start()
 
-    def post_process_input(self, post_data):
-        dirpath = str(post_data, encoding='utf-8')
-        filepath = os.path.join(dirpath, 'hydpy.exchange')
+    def post_process_input(self):
         alpha = None
         lz = None
-        with open(filepath, 'r') as infile:
-            lines = infile.readlines()
-            for line in lines:
-                if line.startswith('alpha'):
-                    alpha = eval(line.split('=')[1])[0]
-                if line.startswith('lz'):
-                    lz = eval(line.split('=')[1])[0]
-                if line.startswith('time'):
-                    time_ = [int(t) for t in eval(line.split('=')[1])]
+        for line in self.server.inlines:
+            if line.startswith('alpha'):
+                alpha = eval(line.split('=')[1])[0]
+            if line.startswith('lz'):
+                lz = eval(line.split('=')[1])[0]
+            if line.startswith('time'):
+                time_ = [int(t) for t in eval(line.split('=')[1])]
         init = pub.timegrids.init
         sim = pub.timegrids.sim
         sim.firstdate = init.firstdate + f'{time_[0]}d'
@@ -172,7 +170,7 @@ class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         if not idx1:
             self.server.hp.conditions = self.server.init_conditions
         else:
-            self.server.hp.conditions = self.server.conditions[dirpath][idx1]
+            self.server.hp.conditions = self.server.conditions[self.server.id_][idx1]
         if alpha is not None:
             for element in self.server.hp.elements.catchment:
                 getattr(element.model.parameters.control, 'alpha')(alpha)
@@ -180,17 +178,17 @@ class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             element = self.server.hp.elements.land_lahn_1
             element.model.sequences.states.lz(lz)
         self.server.hp.doit()
-        self.server.conditions[dirpath][idx2] = self.server.hp.conditions
-        filepath = os.path.join(dirpath, 'hydpy.exchange')
-        with open(filepath, 'w') as outfile:
-            for line in lines:
-                key = line.split('=')[0]
-                if key.endswith('.discharge'):
-                    node = getattr(self.server.hp.nodes, key[:-10])
-                    line = f'{key}={list(node.sequences.sim.series[idx1:idx2])}\n'
-                if 'lz' in key:
-                    line = f'lz = [{self.server.hp.elements.land_lahn_1.model.sequences.states.lz.value}]\n'
-                outfile.write(line)
+        self.server.conditions[self.server.id_][idx2] = self.server.hp.conditions
+        outlines = []
+        for line in self.server.inlines:
+            key = line.split('=')[0]
+            if key.endswith('.discharge'):
+                node = getattr(self.server.hp.nodes, key[:-10])
+                line = f'{key}={list(node.sequences.sim.series[idx1:idx2])}'
+            if 'lz' in key:
+                line = f'lz = [{self.server.hp.elements.land_lahn_1.model.sequences.states.lz.value}]'
+            outlines.append(line)
+        self.server.outlines = outlines
 
 
 class HydPyHTTPServer(http.server.HTTPServer):
@@ -198,7 +196,6 @@ class HydPyHTTPServer(http.server.HTTPServer):
     hp: hydpytools.HydPy
     conditions: collections.defaultdict
     init_conditions: dict
-
 
     def prepare_hydpy(self, projectname):
         self.hp = hydpytools.HydPy(projectname)
@@ -227,7 +224,7 @@ def start_server(
     server.serve_forever()
 
 
-def await_server(port, seconds, *, logfile=None):
+def await_server(port, seconds):
     """
 
     >>> from hydpy import run_subprocess, TestIO

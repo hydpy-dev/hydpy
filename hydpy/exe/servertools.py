1fb07a96e46312460b7f4fe57441c965bcf5fd1b
Christoph Tyralla <Christoph.Tyralla@rub.de>
2018-12-10 22:08:45 +0100

    Complete HTTP request methods of module `servertools`.

--- hydpy/exe/servertools.py
+++ hydpy/exe/servertools.py
@@ -2,6 +2,7 @@
 """
 
 
+
 >>> from hydpy.core.examples import prepare_full_example_1
 >>> prepare_full_example_1()
 
@@ -14,19 +15,20 @@
 ...     run_subprocess('hyd.py await_server 8080 10', verbose=False)
 
 >>> from urllib import request
->>> from hydpy import print_values
+>>> from hydpy import Date, print_values
+>>> t0 = Date('1996-01-01')
 >>> def test(id_, time_, alpha):
 ...     content = (f"alpha= [{alpha}] \\n"
-...                f"refdate= '01 jan 1996' \\n"
-...                f"unit= '1d' \\n"
-...                f"time={time_} \\n"
+...                f"firstdate = {t0+f'{int(time_[0])}d'}\\n"
+...                f"lastdate = {t0+f'{int(time_[2])}d'}\\n"
 ...                f"dill.discharge=[0.0] \\n"
 ...                f"lahn_1.discharge=[0.0]").encode('utf-8')
-...     for methodname in ('period', 'parametervalues', 'load_conditionvalues',
-...                  'conditionvalues', 'simulate', 'save_conditionvalues',
-...                  'itemvalues'):
+...     for methodname in (
+...             'simperiod', 'parameteritems', 'load_conditionvalues',
+...             'conditionitems', 'simulate', 'save_conditionvalues',
+...             'seriesitems'):
 ...         url = f'http://localhost:8080/{methodname}?id={id_}'
-...         if methodname in ('period', 'parametervalues', 'conditionvalues'):
+...         if methodname in ('simperiod', 'parameteritems', 'conditionitems'):
 ...             response = request.urlopen(url, data=content)
 ...         else:
 ...             response = request.urlopen(url)
@@ -122,6 +124,67 @@ state = ServerState()
 
         
 class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    """ToDo
+
+    >>> from hydpy.core.examples import prepare_full_example_1
+    >>> prepare_full_example_1()
+    >>> from hydpy import run_subprocess, TestIO
+    >>> import subprocess
+    >>> with TestIO():
+    ...     process = subprocess.Popen(
+    ...         'hyd.py start_server 8080 LahnH 1996-01-01 1996-01-06 1d',
+    ...         shell=True)
+    ...     run_subprocess('hyd.py await_server 8080 10', verbose=False)
+
+    >>> from urllib import request
+    >>> def test(methodname, data=None):
+    ...     url = f'http://localhost:8080/{methodname}?id=ID'
+    ...     if data is not None:
+    ...         data = bytes(data, encoding='utf-8')
+    ...     print(str(request.urlopen(url, data=data).read(), encoding='utf-8'))
+    >>> test('status')
+    status = ready
+
+    >>> test('parameteritemtypes')
+    alpha = DoubleItem1D(1)
+    >>> test('conditionitemtypes')
+    lz = DoubleItem0D()
+    >>> test('seriesitemtypes')
+    dill = DoubleItem1D(5)
+    lahn_1 = DoubleItem1D(5)
+
+    >>> test('inittimegrid')
+    firstdate = 1996-01-01 00:00:00
+    lastdate = 1996-01-06 00:00:00
+    stepsize = 1d
+
+    >>> test('simperiod',
+    ...      ('firstdate = 1996-01-01 00:00:00\\n'
+    ...       'lastdate = 1996-01-02 00:00:00'))
+    <BLANKLINE>
+
+    >>> test('parameteritems', 'alpha = [3.0]')
+    <BLANKLINE>
+    >>> test('conditionitems', 'lz = [10.0]')
+    <BLANKLINE>
+    >>> test('seriesitems',
+    ...      'dill.discharge = [5.0]\\n'
+    ...      'lahn_1.discharge = [6.0]')
+    <BLANKLINE>
+
+    >>> test('parameteritems')
+    alpha = [3.0]
+    >>> test('conditionitems')
+    lz = [10.0]
+    >>> test('seriesitems')
+    dill.discharge = [5.0]
+    lahn_1.discharge = [6.0]
+
+    >>> test('close_server')
+    shutting down server
+    >>> process.kill()
+    >>> _ = process.communicate()
+    """
 
     def _set_headers(self):
         self.send_response(200)
@@ -226,34 +289,58 @@ class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         shutter.start()
 
     def post_process_input(self):
-        self.post_period()
-        self.post_parametervalues()
+        self.post_simperiod()
+        self.post_parameteritems()
         self.get_load_conditionvalues()
-        self.post_conditionvalues()
+        self.post_conditionitems()
         self.get_simulate()
         self.get_save_conditionvalues()
-        self.get_itemvalues()
+        self.get_parameteritems()
+        self.get_conditionitems()
+        self.get_seriesitems()
 
-    def post_period(self):
-        time_ = eval(state.inputs['time'])
+    def get_parameteritemtypes(self):
+        state.outputs['alpha'] = 'DoubleItem1D(1)'
+
+    def get_conditionitemtypes(self):
+        state.outputs['lz'] = 'DoubleItem0D()'
+
+    def get_seriesitemtypes(self):
+        state.outputs['dill'] = 'DoubleItem1D(5)'
+        state.outputs['lahn_1'] = 'DoubleItem1D(5)'
+
+    def get_inittimegrid(self):
+        init = pub.timegrids.init
+        state.outputs['firstdate'] = init.firstdate
+        state.outputs['lastdate'] = init.lastdate
+        state.outputs['stepsize'] = init.stepsize
+
+    def post_simperiod(self):
         init = pub.timegrids.init
         sim = pub.timegrids.sim
-        sim.firstdate = init.firstdate + f'{int(time_[0])}d'
-        sim.lastdate = init.firstdate + f'{int(time_[2])}d'
+        sim.firstdate = state.inputs['firstdate']
+        sim.lastdate = state.inputs['lastdate']
         state.idx1 = init[sim.firstdate]
         state.idx2 = init[sim.lastdate]
 
-    def post_parametervalues(self):
+    def post_parameteritems(self):
         alpha = state.inputs.get('alpha', None)
         if alpha is not None:
             for element in state.hp.elements.catchment:
-                getattr(element.model.parameters.control, 'alpha')(alpha[1])
+                getattr(
+                    element.model.parameters.control, 'alpha')(eval(alpha)[0])
 
-    def post_conditionvalues(self):
+    def post_conditionitems(self):
         lz = state.inputs.get('lz', None)
         if lz is not None:
             element = state.hp.elements.land_lahn_1
-            element.model.sequences.states.lz(lz)
+            element.model.sequences.states.lz(eval(lz)[0])
+
+    def post_seriesitems(self):
+        state.hp.nodes.dill.sequences.sim.series[state.idx1:state.idx2] = \
+            eval(state.inputs['dill.discharge'])
+        state.hp.nodes.lahn_1.sequences.sim.series[state.idx1:state.idx2] = \
+            eval(state.inputs['lahn_1.discharge'])
 
     def get_load_conditionvalues(self):
         if not state.idx1:
@@ -268,15 +355,21 @@ class HydPyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def get_simulate(self):
         state.hp.doit()
 
-    def get_itemvalues(self):
+    def get_parameteritems(self):
         state.outputs['alpha'] = \
             [state.hp.elements.land_dill.model.parameters.control.alpha.value]
+
+    def get_conditionitems(self):
         state.outputs['lz'] = \
             [state.hp.elements.land_lahn_1.model.sequences.states.lz.value]
+
+    def get_seriesitems(self):
         state.outputs['dill.discharge'] = \
-            list(state.hp.nodes.dill.sequences.sim.series[state.idx1:state.idx2])
+            list(state.hp.nodes.dill.sequences.sim.series
+                 [state.idx1:state.idx2])
         state.outputs['lahn_1.discharge'] = \
-            list(state.hp.nodes.lahn_1.sequences.sim.series[state.idx1:state.idx2])
+            list(state.hp.nodes.lahn_1.sequences.sim.series
+                 [state.idx1:state.idx2])
 
 
 def start_server(

5a2e854f1bb4f8d8b2eb59654e43eefeafa7b260
Christoph Tyralla <Christoph.Tyralla@rub.de>
2019-07-01 12:20:50 +0200

    Add a doctest to method `modify_init` of class `AbsErrorMax` of base model `dam`.

--- hydpy/models/dam/dam_solver.py
+++ hydpy/models/dam/dam_solver.py
@@ -8,13 +8,7 @@ from hydpy.core import parametertools
 
 
 class AbsErrorMax(parametertools.SolverParameter):
-    """Absolute numerical error tolerance [m3/s].
-
-    Note that the default initial value 0.01 refers to mm and the actual
-    simulation step size. Hence the actual default initial value in m³/s is:
-
-    :math:`AbsErrorMax = 0.01 \\cdot CatchmentArea \\cdot 1000 / Seconds`
-    """
+    """Absolute numerical error tolerance [m3/s]."""
     NDIM = 0
     TYPE = float
     TIME = None
@@ -22,6 +16,25 @@ class AbsErrorMax(parametertools.SolverParameter):
     INIT = 0.01
 
     def modify_init(self):
+        """""Adjust and return the value of class constant `INIT`.
+
+        Note that the default initial value 0.01 refers to mm and the
+        actual simulation step size.  Hence the actual default initial
+        value in m³/s is:
+
+        :math:`AbsErrorMax = 0.01 \\cdot CatchmentArea \\cdot 1000 / Seconds`
+
+        >>> from hydpy.models.dam import *
+        >>> parameterstep('1d')
+        >>> simulationstep('1h')
+        >>> solver.abserrormax.INIT
+        0.01
+        >>> catchmentarea(2.0)
+        >>> derived.seconds.update()
+        >>> from hydpy import round_
+        >>> round_(solver.abserrormax.modify_init())
+        0.005556
+        """
         pars = self.subpars.pars
         catchmentarea = pars.control.catchmentarea
         seconds = pars.derived.seconds

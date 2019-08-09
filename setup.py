32d04c511002b7deeffe3e034bc7ca7b0c1d934e
Christoph Tyralla <Christoph.Tyralla@rub.de>
2019-08-09 10:50:44 +0200

    Update version number.

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ with open("README.rst", "r") as readmefile:
 
 # The usual setup definitions.
 setup(name='HydPy',
-      version='4.0a1',
+      version='4.0a2',
       description='A framework for the development and application of '
                   'hydrological models.',
       long_description=long_description,

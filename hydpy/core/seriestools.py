f6b82a331e42c5c0187e7eeaf046a2aaddac7cdf
Christoph Tyralla <c.tyralla@bjoernsen.de>
2020-12-11 09:41:47 +0100

    Add the argument subperiod` to function `aggregate_series` of module `seriestools`.

--- hydpy/core/seriestools.py
+++ hydpy/core/seriestools.py
@@ -13,7 +13,7 @@ import hydpy
 from hydpy.core import exceptiontools
 from hydpy.core import objecttools
 from hydpy.core import timetools
-from hydpy.core import typingtools
+from hydpy.core.typingtools import *
 
 if TYPE_CHECKING:
     import pandas
@@ -24,10 +24,11 @@ else:
 @overload
 def aggregate_series(
     *,
-    series: typingtools.VectorInput[float],
+    series: VectorInput[float],
     stepsize: Literal["daily", "d"],
-    aggregator: Callable[[typingtools.VectorInput[float]], float],
-    basetime: str = ...,
+    aggregator: Union[str, Callable[[VectorInput[float]], float]] = "mean",
+    subperiod: bool = True,
+    basetime: str = "00:00",
 ) -> pandas.DataFrame:
     """sim and obs as arguments, daily aggregation"""
 
@@ -35,18 +36,20 @@ def aggregate_series(
 @overload
 def aggregate_series(
     *,
-    series: typingtools.VectorInput[float],
+    series: VectorInput[float],
     stepsize: Literal["monthly", "m"],
-    aggregator: Callable[[typingtools.VectorInput[float]], float],
+    aggregator: Union[str, Callable[[VectorInput[float]], float]] = "mean",
+    subperiod: bool = True,
 ) -> pandas.DataFrame:
     """sim and obs as arguments, monthly aggregation"""
 
 
 @objecttools.excmessage_decorator("aggregate the given series")
 def aggregate_series(
-    series: typingtools.VectorInput[float],
+    series: VectorInput[float],
     stepsize: Literal["daily", "d", "monthly", "m"] = "monthly",
-    aggregator: Union[str, Callable[[typingtools.VectorInput[float]], float]] = "mean",
+    aggregator: Union[str, Callable[[VectorInput[float]], float]] = "mean",
+    subperiod: bool = True,
     basetime: str = "00:00",
 ) -> pandas.DataFrame:
     """Aggregate the time series on a monthly or daily basis.
@@ -77,13 +80,13 @@ def aggregate_series(
     objects (note that the index addresses the left boundary of each time step:
 
     >>> aggregate_series(series=sim.series)
-    2000-11-01     15.5
-    2000-12-01     46.0
-    2001-01-01     77.0
-    2001-02-01    106.5
-    2001-03-01    136.0
-    2001-04-01    166.5
-    Freq: MS, Name: series, dtype: float64
+                series
+    2000-11-01    15.5
+    2000-12-01    46.0
+    2001-01-01    77.0
+    2001-02-01   106.5
+    2001-03-01   136.0
+    2001-04-01   166.5
 
     The following example shows how to restrict the considered period via
     the |Timegrids.eval_| |Timegrid| of the |Timegrids| object available
@@ -91,9 +94,9 @@ def aggregate_series(
 
     >>> pub.timegrids.eval_.dates = "2001-01-01", "2001-03-01"
     >>> aggregate_series(series=sim.series, aggregator=numpy.sum)
-    2001-01-01    2387.0
-    2001-02-01    2982.0
-    Freq: MS, Name: series, dtype: float64
+                series
+    2001-01-01  2387.0
+    2001-02-01  2982.0
 
     Functions |aggregate_series| raises errors like the following for
     unsuitable functions:
@@ -112,8 +115,8 @@ but 1 was given
 
     >>> pub.timegrids.eval_.dates = "2001-01-01", "2001-02-01"
     >>> aggregate_series(series=sim.series, aggregator="sum")
-    2001-01-01    2387.0
-    Freq: MS, Name: series, dtype: float64
+                series
+    2001-01-01  2387.0
 
     |aggregate_series| raises the following error when the requested function
     does not exist:
@@ -124,23 +127,36 @@ but 1 was given
     ValueError: While trying to aggregate the given series, the following \
 error occurred: Module `numpy` does not provide a function named `Sum`.
 
-    To prevent from wrong conclusions, |aggregate_series| generally ignores all
-    data of incomplete intervals:
+    To prevent from wrong conclusions, |aggregate_series| generally ignores
+    all data of incomplete intervals:
 
-    >>> pub.timegrids = "30.11.2000", "2.04.2001", "1d"
+    >>> pub.timegrids = "2000-11-30", "2001-04-02", "1d"
     >>> node.prepare_simseries()
     >>> sim.series = numpy.arange(30, 152+1)
     >>> sim = node.sequences.sim
     >>> aggregate_series(series=sim.series, aggregator="sum")
-    2000-12-01    1426.0
-    2001-01-01    2387.0
-    2001-02-01    2982.0
-    2001-03-01    4216.0
-    Freq: MS, Name: series, dtype: float64
+                series
+    2000-12-01  1426.0
+    2001-01-01  2387.0
+    2001-02-01  2982.0
+    2001-03-01  4216.0
 
     >>> pub.timegrids.eval_.dates = "2001-01-02", "2001-02-28"
     >>> aggregate_series(series=sim.series)
-    Series([], Name: series, dtype: float64)
+    Empty DataFrame
+    Columns: [series]
+    Index: []
+
+    If you want to analyse the data of the complete initialisation period
+    independently of the state of |Timegrids.eval_|, set argument `subperiod`
+    to |False|:
+
+    >>> aggregate_series(series=sim.series, aggregator="sum", subperiod=False)
+                series
+    2000-12-01  1426.0
+    2001-01-01  2387.0
+    2001-02-01  2982.0
+    2001-03-01  4216.0
 
     The following example shows that even with only one missing value at
     the respective ends of the simulation period, |aggregate_series| does
@@ -151,11 +167,11 @@ error occurred: Module `numpy` does not provide a function named `Sum`.
     >>> node.prepare_simseries()
     >>> sim.series = numpy.arange(2, 180+1)
     >>> aggregate_series(series=node.sequences.sim.series)
-    2000-12-01     46.0
-    2001-01-01     77.0
-    2001-02-01    106.5
-    2001-03-01    136.0
-    Freq: MS, Name: series, dtype: float64
+                series
+    2000-12-01    46.0
+    2001-01-01    77.0
+    2001-02-01   106.5
+    2001-03-01   136.0
 
     Now we prepare a time-grid with an hourly simulation step size, to
     show some examples on daily aggregation:
@@ -170,10 +186,10 @@ error occurred: Module `numpy` does not provide a function named `Sum`.
     22 values of the entire period:
 
     >>> aggregate_series(series=sim.series, stepsize="daily")
+                series
     2000-01-02    14.5
     2000-01-03    38.5
     2000-01-04    62.5
-    Freq: 86400S, Name: series, dtype: float64
 
     If you want the aggregation to start at a different time of the day,
     use the `basetime` argument.  In our example, starting at 22 o'clock
@@ -181,11 +197,11 @@ error occurred: Module `numpy` does not provide a function named `Sum`.
     all available data:
 
     >>> aggregate_series(series=sim.series, stepsize="daily", basetime="22:00")
+                         series
     2000-01-01 22:00:00    12.5
     2000-01-02 22:00:00    36.5
     2000-01-03 22:00:00    60.5
     2000-01-04 22:00:00    84.5
-    Freq: 86400S, Name: series, dtype: float64
 
     So far, the `basetime` argument works for daily aggregation only:
 
@@ -204,11 +220,10 @@ aggregation step size only.
     >>> sim = node.sequences.sim
     >>> sim.series = numpy.arange(1, 1+4)
     >>> aggregate_series(series=sim.series, stepsize="daily")
-    2000-01-01    1.0
-    2000-01-02    2.0
-    2000-01-03    3.0
-    2000-01-04    4.0
-    Freq: 86400S, Name: series, dtype: float64
+                series
+    2000-01-02     2.0
+    2000-01-03     3.0
+    2000-01-04     4.0
 
     >>> pub.timegrids = "01.01.2000 22:00", "05.01.2000 22:00", "2d"
     >>> node.prepare_simseries()
@@ -230,6 +245,7 @@ greater one day.
 error occurred: Argument `stepsize` received value `yearly`, but only the \
 following ones are supported: `monthly` (default) and `daily`.
     """
+    timegrids: timetools.Timegrids = hydpy.pub.timegrids
     if isinstance(aggregator, str):
         try:
             realaggregator = getattr(numpy, aggregator)
@@ -239,67 +255,61 @@ following ones are supported: `monthly` (default) and `daily`.
             ) from None
     else:
         realaggregator = aggregator
-    tg = hydpy.pub.timegrids.eval_
+    tg = timegrids.eval_ if subperiod else timegrids.init
     if tg.stepsize > "1d":
         raise ValueError(
             "Data aggregation is not supported for simulation "
             "step sizes greater one day."
         )
-    if stepsize == "daily":
+    if stepsize in ("d", "daily"):
         rule = "86400s"
         offset = (
             timetools.Date(f"2000-01-01 {basetime}") - timetools.Date("2000-01-01")
         ).seconds
-        firstdate_expanded = tg.firstdate - "1d"
-        lastdate_expanded = tg.lastdate + "1d"
     elif basetime != "00:00":
         raise ValueError(
             "Use the `basetime` argument in combination with "
             "a `daily` aggregation step size only."
         )
-    elif stepsize == "monthly":
+    elif stepsize in ("m", "monthly"):
         rule = "MS"
         offset = 0
-        firstdate_expanded = tg.firstdate - "31d"
-        lastdate_expanded = tg.lastdate + "31d"
     else:
         raise ValueError(
             f"Argument `stepsize` received value `{stepsize}`, but only the "
             f"following ones are supported: `monthly` (default) and `daily`."
         )
     dataframe_orig = pandas.DataFrame()
-    idx0, idx1 = hydpy.pub.timegrids.evalindices
+    idx0, idx1 = timegrids.evalindices if subperiod else timegrids.initindices
     dataframe_orig["series"] = numpy.asarray(series)[idx0:idx1]
     dataframe_orig.index = pandas.date_range(
         start=tg.firstdate.datetime,
         end=(tg.lastdate - tg.stepsize).datetime,
         freq=tg.stepsize.timedelta,
     )
-    dataframe_expanded = dataframe_orig.reindex(
-        pandas.date_range(
-            start=firstdate_expanded.datetime,
-            end=lastdate_expanded.datetime,
-            freq=tg.stepsize.timedelta,
-        ),
-        fill_value=numpy.nan,
-    )
-    resampler = dataframe_expanded.resample(
+    resampler = dataframe_orig.resample(
         rule=rule,
         offset=f"{offset}s",
     )
     try:
-        df_resampled_expanded = resampler.apply(lambda x: realaggregator(x.values))
+        dataframe_resampled = resampler.apply(lambda x: realaggregator(x.values))
     except BaseException:
         objecttools.augment_excmessage(
             f"While trying to perform the aggregation based "
             f"on method `{realaggregator.__name__}`"
         )
-    # noinspection PyUnboundLocalVariable
-    idx0 = df_resampled_expanded.first_valid_index()
-    idx1 = df_resampled_expanded.last_valid_index()
-    if idx0 is None:
-        df_resampled_stripped = pandas.DataFrame()
-        df_resampled_stripped["series"] = numpy.array([], dtype=float)
-    else:
-        df_resampled_stripped = df_resampled_expanded.loc[idx0:idx1]
-    return df_resampled_stripped["series"]
+    for jdx0, date0 in enumerate(dataframe_resampled.index):
+        if date0 >= tg.firstdate:
+            break
+    for jdx1, date1 in enumerate(reversed(dataframe_resampled.index)):
+        date = timetools.Date(date1)
+        if stepsize in ("daily", "d"):
+            date += "1d"
+        else:
+            date = date.beginning_next_month
+        if date <= tg.lastdate:
+            jdx1 = len(dataframe_resampled) - jdx1
+            break
+    # pylint: disable=undefined-loop-variable
+    # the dataframe index above cannot be empty
+    return dataframe_resampled[jdx0:jdx1]

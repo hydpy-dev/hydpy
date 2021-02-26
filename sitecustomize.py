e937e29b811bd3da3d6c77ea1a869d16fb9bcb14
Christoph Tyralla <c.tyralla@bjoernsen.de>
2021-02-26 11:44:23 +0100

    Let `sitecustomize.py` raise a warning instead of an error if `coverage.process_startup` does not find the `.coveragerc` file given by the environment variable `COVERAGE_PROCESS_START`.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,6 +1,12 @@
 import os
+import warnings
 
 if os.environ.get("COVERAGE_PROCESS_START") is not None:
     import coverage
 
-    coverage.process_startup()
+    try:
+        coverage.process_startup()
+    except coverage.misc.CoverageException as exc:
+        warnings.warn(
+            f"Something wrong with how HydPy measures code "
+            f"coverage during testing: {exc}")

861013813304b64cfa5dd1d443a0bffd571fc546
Christoph Tyralla <c.tyralla@bjoernsen.de>
2021-08-06 15:25:37 +0200

    Add some pylint "disable=invalid-all-format" statements until PyCQA/pylint#4711 is fixed.

--- hydpy/models/hland/hland_constants.py
+++ hydpy/models/hland/hland_constants.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# due to pylint bug, see http://phoenix2.yizimg.com/PyCQA/pylint/issues/4711
+# pylint: disable=invalid-all-format
 """Base model |hland| allows dividing subbasins into zones (hydrological response
 units).  It applies the equations related to some processes (for example, interception)
 separately for each zone.  Consequently, parameters as the interception capacity

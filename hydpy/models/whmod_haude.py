369a770f7a93d43185da823f93a4d1844a0a0f83
Christoph Tyralla <c.tyralla@bjoernsen.de>
2022-05-20 19:32:51 +0200

    Entferne die temporären WHMod-Modelle und geben den anderen sprechendere Namen.

--- hydpy/models/whmod_haude.py
+++ hydpy/models/whmod_haude.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long, wildcard-import, unused-wildcard-import
-"""
-External (FAO) reference evaporation with a change process order in the interception
-module.
+"""Entspricht weitgehend dem "Original-WHMod".
 
-Just an experiment.
+Unterschiede:
+
+  * vollständigere Ausgaben
+  * Korrektur Bodenwasserbilanz
+  * Korrektur Beta-Berechnung
 """
 
 # import...
@@ -12,29 +14,27 @@ Just an experiment.
 from hydpy.exe.modelimports import *
 from hydpy.core import masktools
 from hydpy.core import modeltools
-
-# ...from arma
 from hydpy.models.whmod import whmod_model
 from hydpy.models.whmod import whmod_masks
 from hydpy.models.whmod.whmod_constants import *
 
 
 class Model(modeltools.AdHocModel):
-    """Version 3 of WHMod (external evapotranspiration, interception experiment)."""
-
+    """Haude-Dommermuth-Trumpf version of WHMod."""
     INLET_METHODS = ()
     RECEIVER_METHODS = ()
     RUN_METHODS = (
         whmod_model.Calc_NiederschlagRichter_V1,
-        whmod_model.Calc_MaxVerdunstung_V2,
-        whmod_model.Calc_Interzeptionsspeicher_V2,
+        whmod_model.Calc_NiedNachInterz_V1,
+        whmod_model.Calc_InterzeptionsVerdunstung_V1,
         whmod_model.Calc_Seeniederschlag_V1,
         whmod_model.Calc_Oberflaechenabfluss_V1,
         whmod_model.Calc_ZuflussBoden_V1,
         whmod_model.Calc_RelBodenfeuchte_V1,
         whmod_model.Calc_Sickerwasser_V1,
+        whmod_model.Calc_Saettigungsdampfdruckdefizit_V1,
+        whmod_model.Calc_MaxVerdunstung_V1,
         whmod_model.Calc_Bodenverdunstung_V1,
-        whmod_model.Corr_Bodenverdunstung_V1,
         whmod_model.Calc_Seeverdunstung_V1,
         whmod_model.Calc_AktVerdunstung_V1,
         whmod_model.Calc_PotKapilAufstieg_V1,
@@ -42,12 +42,29 @@ class Model(modeltools.AdHocModel):
         whmod_model.Calc_AktBodenwassergehalt_V1,
         whmod_model.Calc_PotGrundwasserneubildung_V1,
         whmod_model.Calc_AktGrundwasserneubildung_V1,
-        whmod_model.Calc_VerzGrundwasserneubildung_Zwischenspeicher_V1,
     )
     ADD_METHODS = ()
     OUTLET_METHODS = ()
     SENDER_METHODS = ()
     SUBMODELS = ()
+
+
+class Masks(masktools.Masks):
+    CLASSES = (
+        whmod_masks.NutzNrMask,
+        whmod_masks.NutzLand,
+        whmod_masks.NutzBoden,
+        whmod_masks.NutzGras,
+        whmod_masks.NutzLaubwald,
+        whmod_masks.NutzMais,
+        whmod_masks.NutzNadelwald,
+        whmod_masks.NutzSommerweizen,
+        whmod_masks.NutzWinterweizen,
+        whmod_masks.NutzZuckerrueben,
+        whmod_masks.NutzVersiegelt,
+        whmod_masks.NutzWasser,
+        whmod_masks.BodenTypMask,
+    )
 
 
 tester = Tester()
--- hydpy/models/whmod_pet.py
+++ hydpy/models/whmod_pet.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long, wildcard-import, unused-wildcard-import
-"""Entspricht weitgehend dem "Original-WHMod".
+"""
+External (FAO) reference evaporation.
 
-Unterschiede:
-
-  * vollständigere Ausgaben
-  * Korrektur Bodenwasserbilanz
-  * Korrektur Beta-Berechnung
+This is the new "official" WHMOD version.
 """
 
 # import...
@@ -14,27 +11,28 @@ Unterschiede:
 from hydpy.exe.modelimports import *
 from hydpy.core import masktools
 from hydpy.core import modeltools
+
+# ...from arma
 from hydpy.models.whmod import whmod_model
 from hydpy.models.whmod import whmod_masks
 from hydpy.models.whmod.whmod_constants import *
 
 
 class Model(modeltools.AdHocModel):
-    """Version 1 of WHMod (Dommermuth-Trumpf)."""
+    """External PET version of WHMod."""
     INLET_METHODS = ()
     RECEIVER_METHODS = ()
     RUN_METHODS = (
         whmod_model.Calc_NiederschlagRichter_V1,
-        whmod_model.Calc_NiedNachInterz_V1,
-        whmod_model.Calc_InterzeptionsVerdunstung_V1,
+        whmod_model.Calc_MaxVerdunstung_V2,
+        whmod_model.Calc_Interzeptionsspeicher_V1,
         whmod_model.Calc_Seeniederschlag_V1,
         whmod_model.Calc_Oberflaechenabfluss_V1,
         whmod_model.Calc_ZuflussBoden_V1,
         whmod_model.Calc_RelBodenfeuchte_V1,
         whmod_model.Calc_Sickerwasser_V1,
-        whmod_model.Calc_Saettigungsdampfdruckdefizit_V1,
-        whmod_model.Calc_MaxVerdunstung_V1,
         whmod_model.Calc_Bodenverdunstung_V1,
+        whmod_model.Corr_Bodenverdunstung_V1,
         whmod_model.Calc_Seeverdunstung_V1,
         whmod_model.Calc_AktVerdunstung_V1,
         whmod_model.Calc_PotKapilAufstieg_V1,
@@ -42,6 +40,7 @@ class Model(modeltools.AdHocModel):
         whmod_model.Calc_AktBodenwassergehalt_V1,
         whmod_model.Calc_PotGrundwasserneubildung_V1,
         whmod_model.Calc_AktGrundwasserneubildung_V1,
+        whmod_model.Calc_VerzGrundwasserneubildung_Zwischenspeicher_V1,
     )
     ADD_METHODS = ()
     OUTLET_METHODS = ()

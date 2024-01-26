hydpy-dev/hydpy

commit 7cc01e89a3af5bc2121d7a6a3b6dea1986ffe0a0

Increase the performance of reading parameters from auxiliary files by caching their compiled instead of plain source code.

hydpy/core/filetools.py
@@ -8,6 +8,7 @@ import os
 import runpy
 import shutil
 import zipfile
+import types
 from typing import *
 
 # ...from site-packages
@@ -846,7 +847,7 @@ class ControlManager(FileManager):
 
     # The following file path to content mapping is used to circumvent reading
     # the same auxiliary control parameter file from disk multiple times.
-    _registry: Dict[str, str] = {}
+    _registry: Dict[str, types.CodeType] = {}
     _workingpath: str = "."
     BASEDIR = "control"
     DEFAULTDIR = "default"
@@ -981,7 +982,11 @@ pass its name or the responsible Element object.
             try:
                 if path not in cls._registry:
                     with open(path) as file_:
-                        cls._registry[path] = file_.read()
+                        cls._registry[path] = compile(
+                            source=file_.read(),
+                            filename=filename,
+                            mode="exec",
+                        )
                 exec(cls._registry[path], {}, info)
             except BaseException:
                 objecttools.augment_excmessage(

commit c6d698539628e2ebe7297d7c32b238fee04b7f6d

When caching the byte code of a control file, pass function `compile` the absolute file path instead of the filename so that `inspect.getsource` can find it later (method  `read2dict` of class `ControlManager` of class `filetools`).
This change fixes 7cc01e89a3af5bc2121d7a6a3b6dea1986ffe0a0, which caused problems when the special functionalities of `CallableParameter`.

hydpy/core/filetools.py
@@ -966,7 +966,9 @@ name or the responsible Element object.
                 if path not in cls._registry:
                     with open(path, encoding=config.ENCODING) as file_:
                         cls._registry[path] = compile(
-                            source=file_.read(), filename=filename, mode="exec"
+                            source=file_.read(),
+                            filename=os.path.abspath(filename),
+                            mode="exec",
                         )
                 exec(cls._registry[path], {}, info)
             except BaseException:
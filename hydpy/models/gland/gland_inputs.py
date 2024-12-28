88a440fb986ba7b1cac1eae650f0c279c36e8761
Christoph Tyralla <c.tyralla@bjoernsen.de>
2024-12-28 14:24:02 +0100

    Finish gland_inputs

--- hydpy/models/gland/gland_inputs.py
+++ hydpy/models/gland/gland_inputs.py
@@ -1,10 +1,12 @@
 # pylint: disable=missing-module-docstring
 
+# import...
+# ...from HydPy
 from hydpy.core import sequencetools
 
 
 class P(sequencetools.InputSequence):
-    """Net Precipitation [mm]."""
+    """Precipitation [mm/T]."""
 
     NDIM, NUMERIC = 0, False
     STANDARD_NAME = sequencetools.StandardInputNames.PRECIPITATION

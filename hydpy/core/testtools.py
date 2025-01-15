hydpy-dev/hydpy

commit 309c5af2aea3d01dcb5a879c84b5a28eb30440ab

Add the model family `HydPy-G` including its application models `gland_gr4`, `gland_gr5`, `gland_gr6`.

hydpy/core/testtools.py
@@ -1925,8 +1925,9 @@ PotentialInterceptionEvaporation
     source = inspect.getsource(method.__call__)
     varnames_source: set[str] = set()
     varnames_candidates: set[str] = set(method.__call__.__code__.co_names)
+    names_builtin = set(dir(builtins))
     for varname in tuple(varnames_candidates):
-        if f"modelutils.{varname}" in source:
+        if (varname in names_builtin) or (f"modelutils.{varname}" in source):
             varnames_candidates.remove(varname)
     for varname, prefix in itertools.product(varnames_candidates, prefixes):
         if f"{prefix}.{varname}" in source:

.travis.yml
@@ -7,7 +7,7 @@ dist: jammy
 matrix:
   include:
     - python: 3.10
-    - python: 3.13
+    - python: 3.12
 
 install:
   - pip install --upgrade pip virtualenv
@@ -17,13 +17,13 @@ script:
   - python prepare_build.py
   - python -m build
   - if [ $TRAVIS_PYTHON_VERSION == "3.10" ] ; then nox -s "doctest(numpy='1')"; fi
-  - if [ $TRAVIS_PYTHON_VERSION == "3.13" ] ; then pip install ghp-import; fi
-  - if [ $TRAVIS_PYTHON_VERSION == "3.13" ] ; then nox -s sphinx; fi
-  - if [ $TRAVIS_PYTHON_VERSION == "3.13" ] ; then nox -s black; fi
-  - if [ $TRAVIS_PYTHON_VERSION == "3.13" ] ; then nox -s mypy; fi
-  - if [ $TRAVIS_PYTHON_VERSION == "3.13" ] ; then nox -s pylint; fi
-  - if [ $TRAVIS_PYTHON_VERSION == "3.13" ] ; then nox -s check_consistency; fi
-  - if [ $TRAVIS_PYTHON_VERSION == "3.13" ] ; then python hydpy/docs/combine_docversions.py ; fi
+  - if [ $TRAVIS_PYTHON_VERSION == "3.12" ] ; then pip install ghp-import; fi
+  - if [ $TRAVIS_PYTHON_VERSION == "3.12" ] ; then nox -s sphinx; fi
+  - if [ $TRAVIS_PYTHON_VERSION == "3.12" ] ; then nox -s black; fi
+  - if [ $TRAVIS_PYTHON_VERSION == "3.12" ] ; then nox -s mypy; fi
+  - if [ $TRAVIS_PYTHON_VERSION == "3.12" ] ; then nox -s pylint; fi
+  - if [ $TRAVIS_PYTHON_VERSION == "3.12" ] ; then nox -s check_consistency; fi
+  - if [ $TRAVIS_PYTHON_VERSION == "3.12" ] ; then python hydpy/docs/combine_docversions.py ; fi
 
 env:
   global:
@@ -31,4 +31,4 @@ env:
     - secure: KkDNiyCWcOtSMHzwmR7WuUM9Z08sigox9fgiRaWtdDTNHGHbNRXCf6Rzr32QlThqu9SwwPd5VnEJkontzrG9uTL2rlEzCKJHp4Ncq0lmCBhOJEM7favpIGwjnWQt4kZZ+G/sy9rfV6rshiOMN2rHRkt1gYzRmHTPCEQQ5eVIb7/YnFO+Gc77FGgkqWJXTfYczUMHNL5F3pfdSUbnnsDoqq3dxZ+NtdY6R87+IOT9fclrsL8zcodJC8N2oyGeTfhRcrZESneVcR/NEx6KhVC8A9/8bWK7kc8cYB6ieLo31uU+Ht7UvDzXmXfZrA8FoRbiDaJ3ktW9RQ7BR0GKGyQm/y/bLUrR7Yo2AXLPrF1DvBt8DYvcnMkJ1sNOrJI+oOpfi7AfdxVJnGbWa8w4D5B6EJSCvQwOj1REXgOf8+VrOKUqZMxPCCDDOhKPSx3/9ng7NAT+yRk7pLqKVtJEEszYs2/wK8hask4JMaz9H06b+Dy5nHnr4RQffXPPwAArSZZdu9IC3/38Lqnk+HhUWZLbKnBMd8UdRbDdZ5aFAkK7ZLseiDaAzCE31FscQbNmwpUPgfxgz/ApWZSV/WHlVZvaNBPkK0DTk5b9iW3WeLOC+qXGYzBKTUHIFQbCKEh40P0pTeDlp0sK3bWnT0VPEXJhk0jEN60RrlB9lwLdSzJg4Gs=
 
 after_success:
-  - if [[ $TRAVIS_PYTHON_VERSION == "3.13" ]]; then python publish_docs_copy.py ; fi
+  - if [[ $TRAVIS_PYTHON_VERSION == "3.12" ]]; then python publish_docs_copy.py ; fi

hydpy/core/sequencetools.py
@@ -421,6 +421,10 @@ class StandardInputNames(StrEnum):
     """Global radiation [W/m²]."""
     INTERCEPTED_WATER_HRU = "intercepted_water_hru"
     """Amount of intercepted water [mm]."""
+    MAXIMUM_AIR_TEMPERATURE = "maximum_air_temperature"
+    """Highest air temperature 2 m above the ground within time interval [°C]."""
+    MINIMUM_AIR_TEMPERATURE = "minimum_air_temperature"
+    """Lowest air temperature 2 m above the ground within time interval [°C]."""
     NORMAL_AIR_TEMPERATURE = "normal_air_temperature"
     """Normal air temperature 2 m above the ground [°C]."""
     NORMAL_EVAPOTRANSPIRATION = "normal_evapotranspiration"

hydpy/cythons/interputils.pxd
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """This module defines the Cython declarations related to module |interptools|."""
 
 from cpython cimport PyObject

hydpy/cythons/modelutils.py
@@ -893,8 +893,8 @@ class PyxWriter:
         add(0, "cimport numpy")
         add(
             0,
-            "from libc.math cimport exp, fabs, log, sin, cos, tan, asin, acos, atan, "
-            "isnan, isinf",
+            "from libc.math cimport exp, fabs, log, sin, cos, tan, tanh, asin, acos, "
+            "atan, isnan, isinf",
         )
         add(0, "from libc.math cimport NAN as nan")
         add(0, "from libc.math cimport INFINITY as inf")
@@ -2851,7 +2851,7 @@ def log(double: float) -> float:
 
 
 def fabs(double: float) -> float:
-    """Cython wrapper for the |math.exp| function of module |math| applied on a single
+    """Cython wrapper for the |math.fabs| function of module |math| applied on a single
     |float| object.
 
     >>> from hydpy.cythons.modelutils import fabs
@@ -2946,6 +2946,20 @@ def atan(double: float) -> float:
     call(123.4)
     """
     return numpy.arctan(double)
+
+
+def tanh(double: float) -> float:
+    """Cython wrapper for the |numpy.tanh| function of module |numpy| applied
+    on a single |float| object.
+
+    >>> from hydpy.cythons.modelutils import tanh
+    >>> from unittest import mock
+    >>> with mock.patch('numpy.tanh') as func:
+    ...     _ = tanh(123.4)
+    >>> func.call_args
+    call(123.4)
+    """
+    return numpy.tanh(double)
 
 
 def isnan(double: float) -> float:

hydpy/docs/bib/refs.bib
@@ -323,6 +323,92 @@
   doi       = {10.1061/JRCEA4.0000425},
 }
 
+@phdthesis{Valery,
+author = {Valéry, Alexis},
+year = {2010},
+month = {01},
+school = {AgroParisTech},
+pages = {},
+title = {Modélisation précipitations débit sous influence nivale : Elaboration d'un module neige et évaluation sur 380 bassins versants}
+}
+
+@article{Turcotte2007,
+author = {Turcotte, Richard and Fortin, L.-G and Fortin, Vincent and Fortin, J.-P and Villeneuve, Jean-Pierre},
+year = {2007},
+month = {08},
+pages = {},
+title = {Operational analysis of the spatial distribution and the temporal evolution of the snowpack water equivalent in southern Québec, Canada},
+volume = {38},
+journal = {Water Policy},
+doi = {10.2166/nh.2007.009}
+}
+
+@TechReport{USACE1956,
+  author      = {United States Army Corps of Engineers},
+  institution = {United States Army.Corps of Engineers. Portland District},
+  title       = {Snow hydrology: Summary report of the snow investigations},
+  year        = {1956},
+  pages       = {437},
+}
+
+@Article{airGR2017,
+title = {The Suite of Lumped {GR} Hydrological Models in an {R}
+  package},
+author = {L. Coron and G. Thirel and O. Delaigue and C. Perrin and
+  V. Andréassian},
+journal = {Environmental Modelling and Software},
+year = {2017},
+pages = {166-171},
+volume = {94},
+doi = {10.1016/j.envsoft.2017.05.002},
+}
+
+@Manual{airGRManual,
+title = {{airGR}: Suite of {GR} Hydrological Models for
+  Precipitation-Runoff Modelling},
+author = {L. Coron and O. Delaigue and G. Thirel and D. Dorchies
+  and C. Perrin and C. Michel},
+journal = {R News},
+year = {2023},
+note = {R package version 1.7.4},
+doi = {10.15454/EX11NA},
+url = {https://CRAN.R-project.org/package=airGR},
+}
+
+@article{Perrin2003,
+author = {Perrin, Charles and Michel, Claude and Andréassian, Vazken},
+year = {2003},
+month = {08},
+pages = {275-289},
+title = {Improvement of a Parsimonious Model for Streamflow Simulation},
+volume = {279},
+journal = {Journal of Hydrology},
+doi = {10.1016/S0022-1694(03)00225-7}
+}
+
+@phdthesis{Moine2008,
+  title={Le bassin versant de surface vu par le souterrain: une voie d'am{\'e}lioration des performances et du r{\'e}alisme des mod{\`e}les pluie-d{\'e}bit?},
+  author={Le Moine, Nicolas},
+  year={2008},
+  school={Doctorat G{\'e}osciences et Ressources Naturelles, Universit{\'e} Pierre et Marie~…}
+}
+
+@article{Pushpalatha2011,
+title = {A downward structural sensitivity analysis of hydrological models to improve low-flow simulation},
+journal = {Journal of Hydrology},
+volume = {411},
+number = {1},
+pages = {66-76},
+year = {2011},
+issn = {0022-1694},
+doi = {https://doi.org/10.1016/j.jhydrol.2011.09.034},
+url = {https://www.sciencedirect.com/science/article/pii/S0022169411006846},
+author = {Raji Pushpalatha and Charles Perrin and Nicolas {Le Moine} and Thibault Mathevet and Vazken Andréassian},
+keywords = {Low flows, Simulation, Lumped model, Model efficiency, Uncertainty, Downward approach},
+abstract = {Summary
+Better simulation and earlier prediction of river low flows are needed for improved water management. Here, a top–down structural analysis to improve a hydrological model in a low-flow simulation perspective is presented. Starting from a simple but efficient rainfall–runoff model (GR5J), we analyse the sensitivity of low-flow simulations to progressive modifications of the model’s structure. These modifications correspond to the introduction of more complex routing schemes and/or the addition of simple representations of groundwater–surface water exchanges. In these tests, we wished to improve low-flow simulation while avoiding performance losses in high-flow conditions, i.e. keeping a general model. In a typical downward modelling perspective, over 60 versions of the model were tested on a large set of French catchments corresponding to various low-flow conditions, and performance was evaluated using criteria emphasising errors in low-flow conditions. The results indicate that several best performing structures yielded quite similar levels of efficiency. The addition of a new flow component to the routing part of the model yielded the most significant improvement. In spite of the close performance of several model structures, we conclude by proposing a modified model version of GR5J with a single additional parameter.}
+}
+
 @Article{Minhas1974,
   author    = {Minhas, B. S. and Parikh, K. S. and Srinivasan, T. N.},
   journal   = {Water Resources Research},
@@ -450,6 +536,12 @@ doi = {10.1007/978-1-4613-3344-9_26}
   year={1969}
 }
 
+@phdthesis{Ficchí2017,
+author = {Ficchí, ndrea},
+year = {2017},
+school = {Université Pierre et Marie Curie - Paris VI},
+title = {An adaptive hydrological model for multiple time-steps : diagnostics and improvements based on fluxes consistency.}
+}
 
 @Comment{jabref-meta: databaseType:bibtex;}
 

commit 338301db9b5529514f7e657be9b569c6030044d7

Exclude builtins from the variables considered by function `check_selectedvariables` of module `testtools`.
This change is already in the master branch but has been squashed into 309c5af2aea3d01dcb5a879c84b5a28eb30440ab accidentally, so a cherry-pick is impossible.

hydpy/core/testtools.py
@@ -1934,8 +1934,9 @@ PotentialInterceptionEvaporation
     source = inspect.getsource(method.__call__)
     varnames_source: set[str] = set()
     varnames_candidates: set[str] = set(method.__call__.__code__.co_names)
+    names_builtin = set(dir(builtins))
     for varname in tuple(varnames_candidates):
-        if f"modelutils.{varname}" in source:
+        if (varname in names_builtin) or (f"modelutils.{varname}" in source):
             varnames_candidates.remove(varname)
     for varname, prefix in itertools.product(varnames_candidates, prefixes):
         if f"{prefix}.{varname}" in source:
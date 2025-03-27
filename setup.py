032b98977e3b35b98596a38ca5f725606e312221
Christoph Tyralla <c.tyralla@bjoernsen.de>
2025-03-27 16:44:10 +0100

    Update version number: 6.1.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extension_modules = [
 
 setuptools.setup(
     name="HydPy",
-    version="6.1.3",
+    version="6.1.4",
     description="A framework for the development and application of hydrological "
     "models.",
     long_description=long_description,

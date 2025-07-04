e71188e5ab84c382aa3aee1a005a35c73ebc0e3b
Christoph Tyralla <c.tyralla@bjoernsen.de>
2025-07-04 19:57:30 +0200

    kinw fluxes

--- hydpy/models/kinw/kinw_fluxes.py
+++ hydpy/models/kinw/kinw_fluxes.py
@@ -19,16 +19,36 @@ class QZA(sequencetools.FluxSequence):
     NDIM, NUMERIC, SPAN = 0, True, (0.0, None)
 
 
+class Inflow(sequencetools.FluxSequence):
+    """Flow into the first channel segment [m³/s]."""
+
+    NDIM, NUMERIC, SPAN = 0, False, (None, None)
+
+
 class QG(sequencetools.FluxSequence):
     """Durchfluss gesamt (total discharge) [m³/s]."""
 
     NDIM, NUMERIC, SPAN = 1, True, (0.0, None)
 
 
+class InternalFlow(sequencetools.FluxSequence):
+    """Flow between the channel segments [m³/s]."""
+
+    NDIM, NUMERIC, SPAN = 1, False, (None, None)
+
+    __HYDPY__DELTA_SEGMENTS__ = -1
+
+
 class QA(sequencetools.FluxSequence):
     """Abfluss aus Gerinnestrecke (outflow out of the channel) [m³/s]."""
 
     NDIM, NUMERIC, SPAN = 0, True, (0.0, None)
+
+
+class Outflow(sequencetools.FluxSequence):
+    """Flow out of the last channel segment [m³/s]."""
+
+    NDIM, NUMERIC, SPAN = 0, False, (None, None)
 
 
 class DH(sequencetools.FluxSequence):

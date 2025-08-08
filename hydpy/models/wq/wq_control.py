b11087baeea23bf34b47b73b51a2ae33769ea611
Tyralla <c.tyralla@bjoernsen.de>
2025-08-08 19:13:13 +0200

    wq_widths_strickler: improve Transitions

--- hydpy/models/wq/wq_control.py
+++ hydpy/models/wq/wq_control.py
@@ -1,7 +1,12 @@
 # pylint: disable=missing-module-docstring
 # import...
+# ...from site-packages
+import numpy
+
 # ...from HydPy
+from hydpy.core import objecttools
 from hydpy.core import parametertools
+from hydpy.core import variabletools
 
 # ...from wq
 from hydpy.models.wq import wq_variables
@@ -50,14 +55,115 @@ class TotalWidths(wq_variables.MixinWidths, parametertools.Parameter):
 
 
 class Transitions(parametertools.Parameter):
-    """The measurement heights that mark the transitions between separately calculated
-    cross section sectors [m]."""
+    """Indexes that mark the transitions between separately calculated cross-section
+    sectors [m].
 
-    NDIM, TYPE, TIME, SPAN = 1, int, None, (0.0, None)
+    According to the Python convention, the index :math:`0` would mark the first, and
+    the index :math:`n - 1` would mark the last height/width pair.  However, precisely
+    these two values are disallowed for reasons we explain in the following.
+
+    Parameter |Transitions| defines the transitions between all neighbouring sectors.
+    Hence, one does not need to specify any value if there is only one sector:
+
+    >>> from hydpy.models.wq import *
+    >>> parameterstep()
+    >>> nmbsectors(1)
+    >>> transitions
+    transitions()
+
+    In such cases, it is okay to pass nothing when using the usual parameter value
+    setting syntax:
+
+    >>> transitions()
+    >>> transitions
+    transitions()
+
+    The number of the required values depends on |NmbSectors|, while the range of the
+    allowed values depends on |NmbWidths|:
+
+    >>> nmbwidths(7)
+    >>> nmbsectors(4)
+    >>> transitions(1, 4, 5)
+    >>> transitions
+    transitions(1, 4, 5)
+
+    The index value :math:`0` is not allowed because there is no sector below the
+    "lowest" height/width pair:
+
+    >>> transitions(0, 4, 6)
+    Traceback (most recent call last):
+    ...
+    ValueError: The smallest possible index value of parameter `transitions` of \
+element `?` is 1, but 0 is given.
+
+    >>> transitions
+    transitions(-999999)
+
+    The same logic holds for the "highest" height/width pair:
+
+    >>> transitions(1, 4, 6)
+    Traceback (most recent call last):
+    ...
+    ValueError: The largest possible index value of parameter `transitions` of element \
+`?` is 5 (NmbWidths - 2), but 6 is given.
+
+    >>> transitions
+    transitions(-999999)
+
+    Besides this, one must ensure that the index values are correctly sorted:
+
+    >>> transitions(1, 4, 4)
+    Traceback (most recent call last):
+    ...
+    ValueError: The index values given to parameter `transitions` of element `?` are \
+not strictly rising (1, 4, and 4).
+
+    >>> transitions
+    transitions(-999999)
+    """
+
+    NDIM, TYPE, TIME, SPAN = 1, int, None, (1, None)
 
     def __hydpy__let_par_set_shape__(self, p: parametertools.NmbParameter, /) -> None:
         if isinstance(p, NmbSectors):
             self.__hydpy__change_shape_if_necessary__((p.value - 1,))
+
+    def __call__(self, *args, **kwargs) -> None:
+        if (self.shape[0] > 0) or args or kwargs:
+            super().__call__(*args, **kwargs)
+            if self.shape[0] > 0:
+                values = self.values
+                if not numpy.all(values[:-1] < values[1:]):
+                    self.values = variabletools.INT_NAN
+                    raise ValueError(
+                        f"The index values given to parameter "
+                        f"{objecttools.elementphrase(self)} are not strictly rising "
+                        f"({objecttools.enumeration(values)})."
+                    )
+                if values[0] < 1:
+                    self.values = variabletools.INT_NAN
+                    raise ValueError(
+                        f"The smallest possible index value of parameter "
+                        f"{objecttools.elementphrase(self)} is 1, but {values[0]} is "
+                        f"given."
+                    )
+                if values[-1] > (max_ := self.subpars.nmbwidths.value - 2):
+                    self.values = variabletools.INT_NAN
+                    raise ValueError(
+                        f"The largest possible index value of parameter "
+                        f"{objecttools.elementphrase(self)} is {max_} (NmbWidths - 2), "
+                        f"but {values[-1]} is given."
+                    )
+
+    def trim(self, lower=None, upper=None) -> bool:
+        """Regular trimming is disabled in favour of the special checks described in the
+        main documentation of parameter |Transitions|."""
+        return False
+
+    def __repr__(self) -> str:
+        if (self.subpars.nmbsectors == 1) and (self.shape[0] == 0):
+            return f"{self.name}()"
+        return super().__repr__()
 
 
 class BottomLevels(wq_variables.MixinTrapezes, parametertools.Parameter):

01e0492fda9ecc31383b06947f6dbbf23f7f22d7
Christoph Tyralla <c.tyralla@bjoernsen.de>
2025-08-16 09:52:23 +0200

    Update version number: 6.2dev6

--- hydpy/__init__.py
+++ hydpy/__init__.py
@@ -118,7 +118,7 @@ from hydpy.exe.servertools import await_server, start_server
 from hydpy.exe.xmltools import XMLInterface, run_simulation, xml_validate
 
 
-__version__ = "6.2dev5"
+__version__ = "6.2dev6"
 
 pub.options = optiontools.Options()
 pub.indexer = indextools.Indexer()

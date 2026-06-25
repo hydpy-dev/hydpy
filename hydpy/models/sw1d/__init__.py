9b48d96f7e9e54792fff20a72ab4c78e5e2cc27b
Christoph Tyralla <c.tyralla@bjoernsen.de>
2026-06-25 17:58:24 +0200

    Add the additional control parameters `NmbZones` and `NmbSegments` "officially" to the base models `dummy` and `NmbSegments`.
    This is necessary to get the documentation build running after the change made by commit 9d3eed46706dfa87c7dbff175f78d4e45a6150f1, but might also bring some other (smaller) advantages.

--- hydpy/models/sw1d/__init__.py
+++ hydpy/models/sw1d/__init__.py
@@ -7,7 +7,12 @@ situations like backwater effects that "hydrological" methods cannot handle well
 from hydpy.auxs.anntools import ANN
 from hydpy.auxs.ppolytools import Poly, PPoly
 from hydpy.exe.modelimports import *
-from hydpy.models.sw1d.sw1d_model import Model
+from hydpy.models.sw1d.sw1d_control import NmbSegments as _NmbSegments
+
+ADDITIONAL_CONTROLPARAMETERS = (_NmbSegments,)  # ToDo: Turn into a model member?
+del _NmbSegments
+
+from hydpy.models.sw1d.sw1d_model import Model  # pylint: disable=wrong-import-position
 
 tester = Tester()
 cythonizer = Cythonizer()

8283ca6cbbed6135b4a20ac10648c209fd4df699
Thomas Recknagel <t.recknagel@bjoernsen.de>
2026-07-28 00:23:36 +0200

    Remove unused import from statcorr_control.py

--- hydpy/models/statcorr/statcorr_control.py
+++ hydpy/models/statcorr/statcorr_control.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring
 
 from hydpy.core import exceptiontools
-from hydpy.core import parametertools, timetools
+from hydpy.core import parametertools
 from hydpy.core.typingtools import *
 
 
